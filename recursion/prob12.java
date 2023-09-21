00e8fbe3a86346dc1914b62ca73e220322007d62
Medhashis_Maiti <medhashis000@gmail.com>
2023-09-21 22:01:08 +0530

    duplicates-google

--- recursion/prob12.java
+++ recursion/prob12.java
@@ -1,7 +1,25 @@
 public class prob12 {
     public static void main(String[] args) {
         // Q . Remove duplicates in a string.
-        
+        String str = "appnacollege";
+        removeDuplicates(str, 0, new StringBuilder("") ,new boolean[26]);
     }
+    public static void removeDuplicates(String str,int idx,StringBuilder newStr,boolean map[]){
+        // base case
+        if(idx==str.length()){
+            System.out.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(map[currChar-'a']==true)
+        {
+            //dulpicate
+            removeDuplicates(str,idx+1,newStr,map);
+
+        }
+        else{
+            map[currChar-'a'] = true;
+            removeDuplicates(str,idx+1,newStr.append(currChar),map);
+        }
     
-}
+    }}
